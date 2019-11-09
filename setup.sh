25e8caf821b2e58437f20e10fe8e98e1dc8917c9
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2019-11-09 11:46:22 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,23 @@
+clear
+apt update
+apt upgrade
+apt install figlet
+apt install toilet
+clear
+termux-setup-storage
+mkdir $PREFIX/Virus
+cp -f bat.zip $PREFIX/Virus
+cp -f shell.zip $PREFIX/Virus
+cp -f com.zip $PREFIX/Virus
+cp -f .Elite.apk $PREFIX/Virus
+mkdir /sdcard/Virus
+cd $PREFIX/Virus
+unzip bat.zip
+unzip shell.zip
+unzip com.zip
+cd ~/Virus-Making
+cp -f virus $PREFIX/bin
+chmod 777 $PREFIX/bin/virus
+rm -Rf *
+clear
+echo "You can start this command :- virus"
